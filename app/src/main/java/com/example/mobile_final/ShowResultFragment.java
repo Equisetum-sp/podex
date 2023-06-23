bce903b503fc63688966304a824a38f06a952fa9
Equisetum-sp <joker.chpy.2@gmail.com>
2023-06-23 15:21:12 +0700

    Fixed main activity xml. Added fragment container, Fixed recyviewtemplate

---
+++ app/src/main/java/com/example/mobile_final/ShowResultFragment.java
@@ -0,0 +1,21 @@
+package com.example.mobile_final;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class ShowResultFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_show_result, container, false);
+    }
+}
