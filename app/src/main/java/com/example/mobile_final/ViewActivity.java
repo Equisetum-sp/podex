0b0d356ba67eff8ee90c6d8dabad50b735f8128e
Equisetum-sp <joker.chpy.2@gmail.com>
2023-06-28 15:01:01 +0700

    Add method and variable to read bundle

--- app/src/main/java/com/example/mobile_final/ViewActivity.java
+++ app/src/main/java/com/example/mobile_final/ViewActivity.java
@@ -2,9 +2,12 @@ package com.example.mobile_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.content.Context;
 import android.widget.TextView;
@@ -40,14 +43,38 @@ Voice Button : viewButtonVoice
 *
 * */
 public class ViewActivity extends AppCompatActivity {
+    public static final String TAG_POKEMON = "Pokemon";
+    public static final String TAG_POKEMONLEGACY = "Pokemon LEGACY";
+
+    Pokemon pokemon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
-        Button PokeCries = findViewById(R.id.viewButtonVoice);
-        PokeCries.setOnClickListener(view -> {
-            MediaPlayer mp = MediaPlayer.create(this, R.raw.pikachucry);
-            mp.start();
-        });
+        Button cry = findViewById(R.id.viewButtonVoice);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent i = getIntent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pokemon = i.getExtras().getParcelable(TAG_POKEMON, Pokemon.class);
+            if (pokemon == null){
+                Log.e("ViewActivity", "NullPointer: Pokemon is null");
+            }
+        }
+        else{
+            int dexno = i.getIntExtra(TAG_POKEMONLEGACY, 1);
+            try {
+                pokemon = PokemonList.getInstance(this).getPokemon(dexno).get(0);
+            }catch (Exception e){
+                Log.e("ViewActivity", e.getMessage());
+                return;
+            }
+        }
     }
 }
