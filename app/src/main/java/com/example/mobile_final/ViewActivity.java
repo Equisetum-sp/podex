8655266c5c47baf6925dbe0c5eadfd7990bfa425
Equisetum-sp <joker.chpy.2@gmail.com>
2023-06-28 16:26:19 +0700

    Implemented method to play sound and show sprite. Fixed view activity

--- app/src/main/java/com/example/mobile_final/ViewActivity.java
+++ app/src/main/java/com/example/mobile_final/ViewActivity.java
@@ -3,13 +3,17 @@ package com.example.mobile_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
+import android.graphics.BlendMode;
+import android.graphics.BlendModeColorFilter;
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.content.Context;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /* NOTES
@@ -46,14 +50,51 @@ public class ViewActivity extends AppCompatActivity {
     public static final String TAG_POKEMON = "Pokemon";
     public static final String TAG_POKEMONLEGACY = "Pokemon LEGACY";
 
+    //region view variables
+    LinearLayout layoutPokemon;
+    TextView dexno, name, about, height, weight;
+    ImageView sprite;
+    //endregion
+
+
     Pokemon pokemon;
+    MediaPlayer soundPlayer;
+
+    private void showPokemonData(){
+        layoutPokemon.getBackground().setColorFilter(new BlendModeColorFilter(pokemon.getTypes()[0].getColor(), BlendMode.SRC_ATOP));
+
+        dexno.setText("#" + String.format("%04d", pokemon.getId()));
+        name.setText(pokemon.getName());
+        //about.setText(pokemon.getAbout());
+        height.setText(Double.toString(pokemon.getHeight()) + " m");
+        weight.setText(Double.toString(pokemon.getWeight()) + " kg");
+
+        sprite.setImageDrawable(pokemon.getSprite(this));
+
+        soundPlayer = pokemon.getSoundPlayer(this);
+    }
+
+    private void buttonHandlerPlaySound(View v){
+        soundPlayer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
-        Button cry = findViewById(R.id.viewButtonVoice);
 
+        layoutPokemon = (LinearLayout) findViewById(R.id.viewLayoutPokemon);
+
+        dexno = (TextView) findViewById(R.id.viewDexNo);
+        name = (TextView) findViewById(R.id.viewName);
+        about = (TextView) findViewById(R.id.viewStatsAbout);
+        height = (TextView) findViewById(R.id.viewHeight);
+        weight = (TextView) findViewById(R.id.viewWeight);
+
+        sprite = (ImageView) findViewById(R.id.viewImg);
+
+        Button cry = findViewById(R.id.viewButtonVoice);
+        cry.setOnClickListener(this::buttonHandlerPlaySound);
 
     }
 
@@ -75,6 +116,20 @@ public class ViewActivity extends AppCompatActivity {
                 Log.e("ViewActivity", e.getMessage());
                 return;
             }
+        }
+
+        if (pokemon != null){
+            showPokemonData();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Release the MediaPlayer resources
+        if (soundPlayer != null) {
+            soundPlayer.release();
+            soundPlayer = null;
         }
     }
 }
